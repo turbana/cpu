631073c26e167d384659eb6c68a3eb1bdbb1b1d1
Ian Clark <turbana@gmail.com>
2013-09-02 14:46:39 -0700

    Added initial disassembler.

---
+++ asm/dis.py
@@ -0,0 +1,18 @@
+import sys
+
+import isa
+
+
+def main(args):
+	if len(args) != 1:
+		print "USAGE: dis file.o"
+		return 2
+	bytes = map(ord, open(args[0], "rb").read())
+	for i in xrange(len(bytes)/2):
+		j = i * 2
+		word = (bytes[j] << 8) | bytes[j+1]
+		print isa.decode(word)
+
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv[1:]))
