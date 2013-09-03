c662643ae4d49db4e9d6530651aab627445af6a4
Ian Clark <turbana@gmail.com>
2013-09-02 20:18:29 -0700

    Added push/pop and call/ret macros.

--- asm/macros.py
+++ asm/macros.py
@@ -8,9 +8,43 @@ def zero(pos, count):
 	return [isa.Number((0, 10), bits=16*words, signed=False)]
 
 
-@macro("reg u16")
+@macro("reg s16")
 def ldi(pos, reg, imm):
 	return """
 		lui  {reg}, (({imm} & 0xFF00) >> 8)
 		addi {reg}, ({imm} & 0x00FF)
 	""".format(reg=reg, imm=imm.value)
+
+
+@macro("reg")
+def push(pos, reg):
+	return """
+		sub $6, $6, 1
+		stw 0($6), {reg}
+	""".format(reg=reg)
+
+
+@macro("reg")
+def pop(pos, reg):
+	return """
+		ldw	{reg}, 0($6)
+		add $6, $6, 1
+	""".format(reg=reg)
+
+
+@macro("u16")
+def call(pos, addr):
+	pos = pos / 2
+	return """
+		.ldi  $5, {pc}
+		.push $5
+		jmp   {addr}
+	""".format(addr=addr.value, pc=pos+5)
+
+
+@macro
+def ret(pos):
+	return """
+		.pop $5
+		jmp  $5
+	"""
