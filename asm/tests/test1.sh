e44f27d79e0f78edc352a97a8124126d88cd01db
Ian Clark <turbana@gmail.com>
2014-11-22 20:52:46 -0800

    Improved output for assembler test1

--- asm/tests/test1.sh
+++ asm/tests/test1.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-egrep -o '; [ 01]* \|' test1.asm | tr -d ' ;|' | fold -w 8 > test1.good.bin
+egrep -o '; [ 01]* \|' test1.asm | tr -d ' ;|' > test1.good.bin
 python ../asm.py test1.asm test1.o
-xxd -b -c 1 test1.o | cut -f2 -d' ' > test1.test.bin
+xxd -b -c 1 test1.o | cut -f2 -d' ' | sed 'N;s/\n//' > test1.test.bin
 diff test1.test.bin test1.good.bin
