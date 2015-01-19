be2ffdb1df753d8e17b2ecb86d99337982bb833c
Ian Clark <turbana@gmail.com>
2015-01-19 13:50:24 -0800

    Update dis-assembler to new binary format

--- asm/dis.py
+++ asm/dis.py
@@ -3,15 +3,47 @@ import sys
 import encoding
 
 
+MAGIC_HEADER = 0xDEADF00D
+
+
 def main(args):
 	if len(args) != 1:
 		print "USAGE: dis file.o"
 		return 2
-	bytes = map(ord, open(args[0], "rb").read())
-	for i in xrange(len(bytes)/2):
-		j = i * 2
-		word = (bytes[j] << 8) | bytes[j+1]
-		print "%04X: (%04X)  %s" % (i, word, encoding.decode(word))
+	out_stream = sys.stdout
+	with open(args[0], "rb") as in_stream:
+		if MAGIC_HEADER != read(in_stream, 4):
+			print "ERROR: Magic header not found"
+			return 1
+		show_data(in_stream, out_stream)
+		show_text(in_stream, out_stream)
+
+
+def read(stream, bytes):
+	value = 0
+	for b in stream.read(bytes):
+		value <<= 8
+		value |= ord(b)
+	return value
+
+
+def show_data(data, out):
+	length = read(data, 2)
+	for addr in range(length):
+		word = read(data, 2)
+		if addr % 8 == 0:
+			out.write("\n%04X:" % addr)
+		out.write(" %04X" % word)
+	if addr % 8 != 0:
+		out.write("\n")
+	out.write("\n")
+
+
+def show_text(data, out):
+	length = read(data, 2)
+	for addr in range(length):
+		word = read(data, 2)
+		out.write("%04X: (%04X)  %s\n" % (addr, word, encoding.decode(word)))
 
 
 if __name__ == "__main__":
