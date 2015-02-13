ca641d7601396a46db562307d974251b2c8ddee9
Ian Clark <turbana@gmail.com>
2015-02-12 17:55:04 -0800

    Updated test file macros.asm to use assembler variables

--- asm/tests/macros.asm
+++ asm/tests/macros.asm
@@ -1,27 +1,32 @@
 ;;
 ;; Test macros in seperate file
 ;;
-;@; stop(55)
+;@; stop(17)
 ;@; assert($1 = 0x00FF)
 ;@; assert($2 = 0x0023)
+;@; assert($3 = 0x0004)
+
+	.set	count, 0
+	.set	latest, 0
 
 	.data
 	.zero	32
 sbp:
 
 	.text
-	.ldi	$7, sbp
-	.call	main, $5
-	jmp	0
+	jmp	main
 
 	.test	a, 0xAB
 	.test	b, 0xBC
 	.test	c, 0xDE
 	.test	d, 0xFF
 
-	.text
-main:	.call	load_a, $6
-	.call	load_b, $6
-	.call	load_c, $6
-	.call	load_d, $6
+load_latest:
+	.ldi	$2, latest
+	ldw	$1, 0($2)
 	.ret	$6
+
+main:	.ldi	$7, sbp
+	.call	latest, $6
+	.ldi	$3, count
+	jmp	0
--- asm/tests/macros.py
+++ asm/tests/macros.py
@@ -3,10 +3,12 @@
 def test(iden, n):
 	return """
 			.data
-	%s:		.dw			%s
+	{name}:	.dw			{value}
 			.text
-	load_%s:
-			.ldi		$2, %s
+	load_{name}:
+			.ldi		$2, {name}
 			ldw			$1, 0($2)
 			.ret		$6
-	""" % (iden, n, iden, iden)
+			.set		latest, load_{name}
+			.set		count, count + 1
+	""".format(name=iden, value=n)
