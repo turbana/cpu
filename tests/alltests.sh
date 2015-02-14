24eced45f864cdfa4102f684d0857fd60c2562b2
Ian Clark <turbana@gmail.com>
2015-02-14 15:42:41 -0800

    Added script to run all tests

---
+++ tests/alltests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEST=./runtest.py
+
+$TEST bigintadd.asm
+$TEST bigintsub.asm
+#$TEST echo.asm
+$TEST fib.asm
+#$TEST macros.asm
+$TEST pow.asm
+#$TEST screen.asm
+#$TEST sections.asm
+#$TEST test1.asm
+#$TEST test2.asm
+#$TEST test3.asm
+#$TEST testimem2.asm
+#$TEST testimem.asm
+#$TEST testisa.asm
+#$TEST timer.asm
