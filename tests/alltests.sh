1db0746204d523c76f7e2554b2a488324f8d87b9
Ian Clark <turbana@gmail.com>
2015-02-15 08:21:50 -0800

    alltests.sh now exits with non-0 when any test fails

--- tests/alltests.sh
+++ tests/alltests.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 TEST=./runtest.py
+status=0
 
-$TEST bigintadd.asm
-$TEST bigintsub.asm
-#$TEST echo.asm
-$TEST fib.asm
-#$TEST macros.asm
-$TEST pow.asm
-#$TEST screen.asm
-#$TEST sections.asm
-#$TEST test1.asm
-#$TEST test2.asm
-#$TEST test3.asm
-#$TEST testimem2.asm
-#$TEST testimem.asm
-$TEST testisa.asm
-#$TEST timer.asm
+run_test() {
+	$TEST $1
+	status=$(($status+$?))
+}
+
+
+run_test bigintadd.asm
+run_test bigintsub.asm
+#run_test echo.asm
+run_test fib.asm
+#run_test macros.asm
+run_test pow.asm
+#run_test screen.asm
+#run_test sections.asm
+#run_test test1.asm
+#run_test test2.asm
+#run_test test3.asm
+#run_test testimem2.asm
+#run_test testimem.asm
+run_test testisa.asm
+#run_test timer.asm
+
+exit $status
