74909cddb025a3a5915a1f3ee5a017278b436e1e
Ian Clark <turbana@gmail.com>
2016-02-03 20:11:24 -0800

    fix_schematic.py now ensures i/opad-2.sym have their net= and refdes= set

--- design/fix_schematic.py
+++ design/fix_schematic.py
@@ -1,5 +1,7 @@
 """
 Update schematic nets from the form A0 to A[0]. This allows us to use gschem's auto text numbering (which doesn't work with A[0]) and also benfit from bus netlisting (which needs A[0]).
+
+Also ensure ipad-2.sym have their net's set
 """
 
 import sys
@@ -18,7 +20,23 @@ def main(args):
         for net in schem.findall(type="net", attr=search):
             num = net.netname[len(name):]
             net.netname = "%s[%s]" %  (name, num)
+    for pad in schem.findall(type="component", basename="[io]pad-2.sym"):
+        value = "%s:1" % pad.netlabel
+        set_attribute(pad, "refdes", value)
+        set_attribute(pad, "net", value)
     schem.save(open(args[1], "w"))
+
+
+def set_attribute(pad, attr, value):
+    try:
+        setattr(pad, attr, value)
+    except AttributeError:
+        new_attr = dict(pad["attributes"][0])
+        del new_attr["text"]
+        new_attr["visibility"] = 0
+        attribute = schematic.object(**new_attr)
+        attribute["text"] = "%s=%s" % (attr, value)
+        pad["attributes"].append(attribute)
 
 
 if __name__ == "__main__":
