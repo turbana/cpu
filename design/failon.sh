51c6bcabc996adb73baa448323c18127ff8e48b7
Ian Clark <turbana@gmail.com>
2016-03-16 17:47:38 -0700

    Added failon.sh to check for compiler output we should fail on

---
+++ design/failon.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+tmp=/tmp/$$.tmp
+trap 'rm -f $tmp' EXIT
+
+if [[ $# -ne 1 ]]; then
+    echo "USAGE: $(basename $0) pattern"
+    echo "Copies stdin to stdout and exits with failure when pattern matches any line"
+    exit 2
+fi
+
+tee $tmp
+exit $(grep -c "$1" $tmp)
