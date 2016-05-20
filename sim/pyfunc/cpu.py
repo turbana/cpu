baa4bc873c2f59cfc96739beb7103fcd53b0a436
Ian Clark <turbana@gmail.com>
2016-05-20 16:37:08 -0700

    cpu.py now stalls when reading after either ldw or ldiw (was only ldw)

--- sim/pyfunc/cpu.py
+++ sim/pyfunc/cpu.py
@@ -330,7 +330,7 @@ class CPU(object):
 		read_args = [str(arg) for arg in token.args if not arg.dest]
 		if self.mem_write_reg in read_args:
 			self.clock += 1
-		self.mem_write_reg = str(token.tgt) if token.name == "ldw" else None
+		self.mem_write_reg = str(token.tgt) if token.name in ("ldw", "ldiw") else None
 
 	@send_listeners
 	def do_interrupt(self, irq):
