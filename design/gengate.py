06043a45a86b0ce28e318534f4bc0fa48a3ffe52
Ian Clark <turbana@gmail.com>
2016-06-17 11:32:58 -0700

    gengate.py no longer escapes values (ex 1'b1)

--- design/gengate.py
+++ design/gengate.py
@@ -13,7 +13,7 @@ import collections
 DEFINITIONS = "74HCxxx.json"
 
 _SPACE_CHARS		= "(){},;"
-_BASIC_WIRE_REGEX	= "[0-9A-Z]+"
+_BASIC_WIRE_REGEX	= "[0-9A-Z]+(?!')"
 _WIRE_REGEX			= "\??%s" % _BASIC_WIRE_REGEX
 _EXPR_REGEX			= "\?%s" % _BASIC_WIRE_REGEX
 
