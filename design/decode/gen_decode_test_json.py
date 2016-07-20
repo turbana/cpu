0d8e3adb111316a997ea68333df7f88fde19a4b2
Ian Clark <turbana@gmail.com>
2016-07-20 15:51:36 -0700

    Added scripts to generate verilog module and test config from decode searcher output.

---
+++ design/decode/gen_decode_test_json.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+
+import csv
+import json
+import math
+import re
+import sys
+
+import genverilog
+
+DONTCARE = 0
+
+bit_widths = {
+	"D_Op": 3,
+	"D_RFa": 4,
+	"D_RFb": 4,
+	"D_Rd": 4,
+	"D_Ra": 4,
+	"D_Rb": 4,
+	"D_Rc": 4,
+	"D_A": 16,
+	"D_B": 16,
+	"D_C": 16,
+	"D_SF": 3,
+}
+
+
+def main(args):
+	if len(args) != 2:
+		print "USAGE: %s decode.csv best.log" % sys.argv[0]
+		return 2
+	csv_filename, log_filename = args
+	table = read_csv(csv_filename)
+	solution = read_log(log_filename)
+	testcase = gen_testcase(table, solution)
+	print json.dumps({"values": testcase}, indent=4)
+
+
+def read_csv(filename):
+	return list(csv.reader(open(filename)))
+	for i, inst in enumerate(table[0]):
+		table[0][i] = clean_wire(inst)
+	for i in range(len(table)):
+		table[i][0] = clean_keyword(table[i][0])
+	return table
+
+
+def read_log(filename):
+	data = genverilog.parse(open(filename))
+	for inst, opcode in data["encoding"].items():
+		new_inst = clean_keyword(inst)
+		if new_inst != inst:
+			del data["encoding"][inst]
+			data["encoding"][new_inst] = opcode
+	return data
+
+
+def clean_keyword(inst):
+	if inst == "and": inst = "_and"
+	if inst == "or":  inst = "_or"
+	return inst.replace(".", "_")
+
+
+def clean_wire(name):
+	return name.replace("[", "").replace("]", "")
+
+
+def clean_value(val):
+	if val == DONTCARE or val == "X":
+		val = "DONTCARE"
+	return val
+
+
+def rewrite_muxes(muxes):
+	wires = {}
+	for mux, values in muxes.items():
+		pass
+
+
+def bit_width(stem, tests):
+	def match((other, _)):
+		return other.startswith(stem) and other[-1] in "0123456789"
+	return sum(map(match, tests))
+
+
+def gen_testcase(table, solution):
+	tests = [
+		("sign", "lambda n, b: n-(2**b) if 2**(b-1) <= n else n"),
+		("slice", "lambda start, size: (FD_I>>start) & (2**size)-1"),
+		("op5", "FD_I >> 11"),
+		("I", "[int(b) for b in reversed(bin(FD_I)[2:].zfill(16))]"),
+		("s4imm", "sign(slice(7, 4), 4)"),
+		("s7imm", "sign(slice(6, 7), 7)"),
+		("s8imm", "sign(slice(3, 8), 8)"),
+		("s11imm", "sign(slice(0, 11), 11)"),
+		("SPI", "s4imm+1 if s4imm>=0 else s4imm"),
+		("RFB_I", "SPI if I[6]==1 else R_B"),
+	]
+	exports = [clean_wire(w) for w in table.pop(0)[1:]]
+	insts = [clean_keyword(row[0]) for row in table]
+	table = [row[1:] for row in table]
+	tests += gen_opcodes(solution)
+	tests += gen_instructions(exports, insts, table)
+	tests += gen_exports(exports)
+	return ["%s = %s" % (name, expr) for name, expr in tests]
+
+
+def gen_opcodes(solution):
+	tests = []
+	for op,  value in sorted(solution["encoding"].items()):
+		code = int(value, 2)
+		tests.append((op, "op5 == %s" % code))
+	return tests
+
+
+def gen_instructions(exports, insts, values):
+	tests = []
+	for col, wire in enumerate(exports):
+		expr = ""
+		for row, op in enumerate(insts):
+			val = clean_value(values[row][col])
+			expr += "%s if %s else " % (val, op)
+		expr += "None"
+		tests.append((wire, expr))
+		# if we're finishing with a wire: generate the concatenated version
+		if wire.endswith("0"):
+			stem = wire[:-1]
+			width = bit_width(stem, tests)
+			expr = " | ".join("(%s%d<<%d)" % (stem, b, b) for b in range(width))
+			expr += " if DONTCARE not in (%s) else DONTCARE" % ",".join("%s%d" % (stem, b) for b in range(width))
+			tests.append((stem, expr))
+	return tests
+
+
+def gen_exports(exports):
+	tests = []
+	stems = set(re.sub("[0-9]", "", wire) for wire in exports)
+	for stem in stems:
+		width = bit_widths.get(stem, 1)
+		tests.append(("%s:%d" % (stem, width), stem))
+	return tests
+
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv[1:]))
---
+++ design/decode/gen_decode_verilog.py
@@ -0,0 +1,165 @@
+""" Generate verilog module from encoding searcher results."""
+
+import math
+import re
+import sys
+
+
+PARAMETERS = {
+	"FD_I":		("input",	16),
+	"R_A":		("input",	16),
+	"R_B":		("input",	16),
+	"D_A":		("output",	16),
+	"D_B":		("output",	16),
+	"D_C":		("output",	16),
+	"D_Ca":		("output",	1),
+	"D_H":      ("output",  1),
+	"D_J":		("output",	1),
+	"D_Mr":		("output",	1),
+	"D_Mt":		("output",	1),
+	"D_Mw":		("output",	1),
+	"D_Op":		("output",	3),
+	"D_RFa":	("output",	4),
+	"D_RFb":	("output",	4),
+	"D_Ra":		("output",	4),
+	"D_Rb":		("output",	4),
+	"D_Rc":		("output",	4),
+	"D_Rd":     ("output",  4),
+	"D_S":		("output",	1),
+	"D_SF":		("output",	3),
+	"D_T":		("output",	1),
+}
+
+ALIASES = {
+	"I": ("FD_I", 16),
+	"s7imm": ("{{9{I[12]}}, I[12-:7]}", 16),
+	"s8imm": ("{{8{I[10]}}, I[10-:8]}", 16),
+	"s11imm": ("{{8{I[10]}}, I[10-:11]}", 16),
+	"spi": ("{{12{I[10]}},I[10-:4]}", 16),
+	"SPI": ("(spi[15]) ? spi : spi+1", 16),
+	"RFB_I": ("(I[6]) ? SPI : R_B", 16),
+}
+
+
+def main(args):
+	if len(args) != 1:
+		sys.stderr.write("USAGE: %s results.log\n" % sys.argv[0])
+		return 2
+	filename = args[0]
+	data = parse(open(filename))
+	emit_module(data)
+
+
+def parse(stream):
+	data = {
+		"encoding": {},
+		"muxes": {},
+		"wires": {},
+	}
+	for line in section(stream):
+		inst, value = line.split()
+		data["encoding"][inst] = value
+	for line in section(stream):
+		name, mux = line.split("=")
+		mux = mux.strip()[6:-1].split(",")
+		mux = [s if s!="X" else 0 for s in mux]
+		data["muxes"][name.strip()] = mux
+	for line in section(stream):
+		name, value = line.split("=")
+		data["wires"][name.strip()] = value.strip()
+	return data
+
+
+def section(stream):
+	for line in stream:
+		line = line.strip()
+		if not line:
+			raise StopIteration()
+		else:
+			yield line
+
+
+def lookup_name(name):
+	if "[" not in name:
+		return name
+	name = name.replace("[", "").replace("]", "")
+	i = name.rfind("_")
+	return "m_%s_%s" % (name[:i], name[i+1:])
+
+
+def lookup_wires(data):
+	all_names = set(data["wires"].keys() + data["muxes"].keys() + ALIASES.keys())
+	names = set(map(wire_name, all_names))
+	_size = lambda n: wire_size(n, all_names)
+	return sorted(zip(names, map(_size, names)))
+
+
+def wire_name(name):
+	if "[" not in name:
+		return name
+	return name.split("[")[0]
+
+
+def wire_size(name, names):
+	def index(n):
+		if "[" not in n:
+			return 0
+		return int(n.replace("]", "").split("[")[1])
+	if name in PARAMETERS:
+		return PARAMETERS[name][1]
+	match = re.compile("^%s\W" % name).match
+	matches = [n for n in names if match(n)]
+	if not matches:
+		if name in ALIASES:
+			return ALIASES[name][1]
+		return 1
+	return max(index(n)+1 for n in matches)
+
+
+def emit_module(data):
+	e = sys.stdout.write
+	e("`timescale 1 ns / 100 ps\n\n")
+	e("module decode_decode (")
+	e(",".join(PARAMETERS))
+	e(");\n\n")
+
+	e("/* port directions */\n")
+	for name, (dir, size) in PARAMETERS.items():
+		if dir == "ignore": continue
+		width = "" if size == 1 else "[%d:0]"%(size-1)
+		e("%s %s %s;\n" % (dir, width, name))
+
+	e("\n/* wires */\n")
+	for wire, size in lookup_wires(data):
+		width = "" if size == 1 else "[%d:0]"%(size-1)
+		e("wire %s %s;\n" % (width, wire))
+
+	e("\n/* continuous assignments */\n")
+	for wire, value in sorted(data["wires"].items()):
+		if not value: value = "0"
+		e("assign %s = %s;\n" % (wire, value))
+
+	e("\n/* aliases */\n")
+	for alias, (value, size) in ALIASES.items():
+		e("assign %s = %s;\n" % (alias, value))
+
+	e("\n/* muxes */\n")
+	for name, mux in sorted(data["muxes"].items()):
+		if len(mux) == 1:
+			e("assign %s = %s;\n" % (name, mux[0]))
+			continue
+		lhs = "assign %s = " % name
+		join = "\n" + " "*len(lhs)
+		bits = int(math.log(len(mux), 2))
+		name = name.replace("[", "").replace("]", "")
+		var = "{%s}" % ",".join("m_%s_%d" % (name, i) for i in reversed(range(bits)))
+		e(lhs)
+		e(join.join("(%s == %d) ? %s :" % (var, i, val)
+					# for i, val in enumerate(mux)))
+					for i, val in enumerate(reversed(mux))))
+		e(" Z;\n")
+	e("\nendmodule\n")
+
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv[1:]))
