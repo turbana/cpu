e3b41ff6b5b7cf49d8036d1eaca110dfdc41d0f1
Ian Clark <turbana@gmail.com>
2016-07-20 15:41:36 -0700

    Reordered ISA opcodes slightly

--- asm/isa.py
+++ asm/isa.py
@@ -39,26 +39,26 @@ def parse_format(ast):
 add("add tgt:reg, op1:reg, op2:ireg",
 	0b00000, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("sub tgt:reg, op1:reg, op2:ireg",
+add("and tgt:reg, op1:reg, op2:ireg",
 	0b00001, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("and tgt:reg, op1:reg, op2:ireg",
+add("or tgt:reg, op1:reg, op2:ireg",
 	0b00010, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("or tgt:reg, op1:reg, op2:ireg",
+add("xor tgt:reg, op1:reg, op2:ireg",
 	0b00011, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("shl tgt:reg, op1:reg, op2:ireg",
+add("shr tgt:reg, op1:reg, op2:ireg",
 	0b00100, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("shr tgt:reg, op1:reg, op2:ireg",
+add("sar tgt:reg, op1:reg, op2:ireg",
 	0b00101, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("sar tgt:reg, op1:reg, op2:ireg",
+add("shl tgt:reg, op1:reg, op2:ireg",
 	0b00110, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
-add("xor tgt:reg, op1:reg, op2:ireg",
-	0b00111, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
+add("sex tgt:reg, src:reg",
+	0b00111, 11, ("src", 5, 3), ("tgt", 2, 0))
 
 add("s.cond:cond op1:reg, op2:ireg",
 	0b01000, 11, ("op2", 10, 6), ("op1", 5, 3), ("cond", 2, 0))
@@ -78,8 +78,8 @@ add("ldw tgt:reg, index:ireg(base:reg)",
 add("ldiw tgt:reg, index:ireg(base:reg)",
 	0b01101, 11, ("index", 10, 6), ("base", 5, 3), ("tgt", 2, 0))
 
-add("sex tgt:reg, src:reg",
-	0b01110, 11, ("src", 5, 3), ("tgt", 2, 0))
+add("sub tgt:reg, op1:reg, op2:ireg",
+	0b01110, 11, ("op2", 10, 6), ("op1", 5, 3), ("tgt", 2, 0))
 
 add("stiw index:spec_imm(base:reg), src:reg",
 	0b01111, 11, ("index", 10, 7), ("base", 5, 3), ("src", 2, 0))
